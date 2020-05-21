f63adcb9e17412dd3f6a3aa04979382c20d4da1f
Oğuz Alp DURAN <30664517+alienozi@users.noreply.github.com>
2020-05-22 01:53:26 +0300

    Add files via upload

---
+++ leexcel.py
@@ -0,0 +1,94 @@
+#PROGRAM WRITTEN BY OGUZ ALP "ALIENOZI" DURAN. 
+# https://alienozi.c1.biz for source
+#++++++++++++++/+++++/+//+/+++++++++++++++++++oooossyyyyysssooooo+++++++++++++++++o++oooooooooooooooo
+#++++++++++++++++++++++++++++++++++++++++ooosssyyyyssoooo+++++++++++++++++++++++++++++++++++ooooooooo
+#+++++++++++++++++++++++++++++++++ooooossyyysssooo++++//////////////////////////++++++++++++++++++ooo
+#+++++++++++++++++++++++++++oooosssyyysssoo+++///////////////////////////////////////+++++++++++++++o
+#++++++++++++++++++++ooooosssyyyssooo+++////////::::::::::::::::::::::::////////////////+++++++++++++
+#++++++++++++ooooooosssyyyssoo+++///////::::::::::::::::::::::::::::::::::::///////////////++++++++++
+#ooooooooooooosssyyyssoo+++//////::::::::::::----------------------:::::::::::::////////////+++++++++
+#oooooosssyyyysssoo+++/////::::::::::---------------------------------::::::::::::////////////+++++++
+#sssyyyyyssooo+++/////::::::::---------------------------------------:-::-:::///::::////////////+++++
+#yyyssooo+++//////::::::::--------------.............-..-..--.-------::/oo+::y/s::::::////////////+++
+#ooo++++///////:::::::------------...................-:-/:-+::-----:+//+yso/ysoy+/+/:::////////////++
+#++++///////::::::----------.......................---:/osssyoo++///+ssysyhyhsyyosyo/:::////////////+
+#+///////:::::::---------.................```````..:+++/ooshddddhssssshhyyyhhhhhyhh+:::::////////////
+#/////:::::::---------..............``````````````.++ys//+sooyhhhddhhdmmmdyyhdddhdds///:::://////////
+#///:::::::---------.............`````````````````-sso//o+sysyhhddddddmmmmmhddmmddddys///////////////
+#/::::::::---------..........```````````````````.:+++//o+oosyhhhdddddddddddddmmmmdddddhso////////////
+#:::::::---------...........````````````````````/+/+ooo++++osyyhhhhhhhyyyyyyhdddddmmmmddho///////////
+#::::::---------............```````````````````::/+oss+////+ooosssssssooooooosyyhhdmmmdddh///:///////
+#:::::----------...........```````````````````:/:://+////////+++++ooooo+++++++osssshdmmddds/:////////
+#::::-----------...........``````````````````/+/-..-..:://///////++++++++++++++oooosyhdmddd+/////////
+#::::------------..........`````````````````.o+:-.....-:////////++++++++++++++++ooooosydmmds/:///////
+#::::------------............```````````````:o+/-.....--:://////++++++++++++++++oooooosydmdh+////////
+#::::-------------...............``````````.soo+:-..``..-://////++++++++++++++++++ooooosyddds////////
+#:::::-------------.................```````:syyo/-```.-://++++++o++++o+++++++++++++++oooyhddy////////
+#::::::---------------.....................:yhho:..-:/ossyhhhhyyysoooooossssssoooo+++oooshddd+///////
+#:::::::---------------....................ohdy/-:oyhhddddmddddhhhsooosyhhdhdddddhyyssooshddd+///////
+#:::::::-------------------................shh/-:oyhddddddddddddhys+++syhhhddddddddddhhysymdh////////
+#::::::::::-------------------............:yhs.-oyhhhyhmNmmdddddhs+//+osyhhdddddddddhhhyyymdy////////
+#::::::::::::-----------------------..---.:yy:.:osssssydmmmhdddhs+///+osyhhddmmmmdhyhdhyssdmy////////
+#:::::::::::::---------------------------:/s+..--:/++osyyyyyyysoo+.-/+osyyhdddNNNmhhhyhysshmy////////
+#::::::::::::::::::::::::::--:----:-----/sss-....--:/++ooooo+++++:../+osssyyhhhhhhyysssssoydy////////
+#//+++++++++++++++++++++++++++++++o+++++os/o-.------::///+++++++/.`-/+oosssosssssssssssooosdh////////
+#+++++++++++++++++++++++++++++++++++++++o+oo--::/:::::://+++++++/.-:/++oossoooooooooooooooohhs+//////
+#soooooooooooooooooooo++++++++++++oooooo+:oo::///::::///+++++o+/--:/++++ossssoooooo+oooooooyhhs//////
+#hyyssssoooooooooooooooooooooooooooooooo+://://////++++++ooooo::/+++ooooosssssoooo++oooooooyyyo//////
+#hhhhyyyyyyyssssssssooooooooooooooooosyys---:/+//++++++oooooo+/+yyyyyysyyssyyssooooooooooosyss///////
+#dddhhhhhhhhhhhyyyyyyyyyyyyyyyyyyyyyyhddd/-/:/+++++++ooossso+oydmdhhhhhhhysyyyssoooooooosssyss///////
+#mddddddddddhhhhhhhhhhhhhhhhhhhhhhhhhhhddsos+/+++++++ooosoo+++oyhhdhdhhhdyssyyyysssssssssssys+///////
+#mmmmmmmmmmmddddddddddddddddddddddddddddddhds/++++++ooooo+++//+ossyhdhyyysssssyyyssssssssssso////////
+#NNmmNmNNmmmmmmmmmmmdmmddddddddddddmmdmmmmmmy/+++++++++/+++++oosssyyhhyyyyysssssyyysssssssss+////////
+#NNNNNNNNNNmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNd//+++++++/++oooossssssyyyyyyyyyssssssyssssssss+/////////
+#NNNNNNNNNNNNmmNNNNNmmmmmmmmmmmmmNNNNNNNNNNNm/+++/++++osyyysssssssssoosssyyyyyyssssssssssoo+///////++
+#NNNNNNNNNNNNNNNNNNNNNmmNNmNNNNNNNNNNNNNNNNNN+++++++osyyhhhhhhhhhdhhhhhhhhhhhhyyyssssssss+////////+++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNy++++ooosyyssosssssssyyhhhhyyhhhhhhyssssssso///////+++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNh+ooooossssssooosssssyssyyyyyyyssyyyyssssss+//////++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNs+oooooosssssssssyyyhhhhhhhyyyysssyyysssyso/////++++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN+/osoooossssssssyyyhhhhhhhhyyyyyyyyyyssyys+//+++++++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNm///ssosoosssssssyyyssssyyyyyyyyyyyyyyyhyy+++++++++++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNh///+sssoooooosssssoooooooosssyyyyyyyyyyyo+++++++++++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNMNNNNNNNNNNo////osssoooooooooooooooosossssssyyyyyyyyo+++++++++++++++
+#NNNNNNNNNNNNNNNNNNNNNNNNNMMNMMMMNNNNNNMNNNm///+++oyysooooooooooosssssssssssssyyyyhys++++++++++++++++
+
+
+import openpyxl
+import random
+
+leexcel = openpyxl.load_workbook('test.xlsx')                                 #opens the file test.xlsx
+
+mainsheet = leexcel.get_sheet_by_name("Veriler")                              #sets Veriler as mainsheet
+
+studentnum = int(input("Öğrenci hücresi nerede bitiyor (ör: C4  ise 4)? "))   #gets student number and prints 
+print ("Öğrenci Sayısı", studentnum, "olarak ayarlandı")
+y = int(input("Ödev hücresi nerede bitiyor? "))                               #gets homework number and prints
+
+                                                                               #ihatemylife = "{}".format(y)
+                                                                               #homework = mainsheet[ 'C2' : ihatemylife]
+
+homework= [mainsheet.cell(row=i, column=3).value for i in range(2, y+1)]
+data= [mainsheet.cell(row=i, column=1).value for i in range(2, studentnum+1)]
+
+workingsheet = leexcel.get_sheet_by_name("Sonuclar")
+
+print(data)
+print(homework)
+
+p= 2
+
+for c1, r1 in zip(data, homework):                                         #prints results
+        r1 = random.choice(homework)
+        print("{}".format(c1), "{}".format(r1))
+        workingsheet.cell(row=p , column= 4).value = r1
+        p = p+1
+
+        continue
+
+leexcel.save('test.xlsx')
+
+print("Ödevler dağıtıldı! Excel sayfa 2 ye bakınız.")
+
+
+                   
+
